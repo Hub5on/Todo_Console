563c4ef6de5cbf5f75324eb525977fdfa3c7c75c
Hub5on <haibert.hs32@gmail.com>
2023-10-18 18:11:11 +0200

    add: implement task addition and input handling

--- Todo.java
+++ Todo.java
@@ -1,42 +1,46 @@
 import java.util.Scanner;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 public class Todo {
     public static void main(String[] args) {
-        
-        Boolean exitAplication = false;
-        int choice = 0;
-        
+        Boolean exitApplication = false;
+        Dictionary<String, Boolean> tasks = new Hashtable<String, Boolean>();
 
-        do{
-            try(Scanner scanner = new Scanner(System.in)){
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
                 System.out.print("\033[H\033[2J");
                 System.out.flush();
                 menu();
                 System.out.print("> ");
-                try{
-                    choice = scanner.nextInt();
-                }catch(Exception e){
-                    System.out.println("Error: " + e.getMessage());
-                } 
-            }
-            switch (choice) {
-                case 1:
-                    
-                    break;
-                case 2:
 
-                    break;
-                case 3:
-
-                    break;
-                case 4:
-                    exitAplication = true;
-                    break;
-            }
-            
-            
-        }while(!exitAplication);
-        
+                try {
+                    int choice = scanner.nextInt();
+                    scanner.nextLine();
+                    switch (choice) {
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            System.out.print("\033[H\033[2J");
+                            System.out.flush();
+                            addNewTask(scanner, tasks);
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            exitApplication = true;
+                            break;
+                        default:
+                            System.out.println("Błędny wybór. Wprowadź poprawną liczbę.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Błąd: " + e.getMessage());
+                    scanner.nextLine();
+                }
+            } while (!exitApplication);
+        }
     }
 
     public static void menu() {
@@ -46,4 +50,15 @@ public class Todo {
         System.out.println("3. Delete task");
         System.out.println("4. Exit");
     }
+
+    public static void addNewTask(Scanner scanner, Dictionary<String, Boolean> tasks) {
+        System.out.println("Write your task todo (or type 'exit' to finish): ");
+        System.out.print("> ");
+        String task = scanner.nextLine();
+        if ("exit".equalsIgnoreCase(task)) {
+            return;
+        }
+        tasks.put(task, false);
+    }
+    
 }
