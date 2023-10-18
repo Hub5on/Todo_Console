d980b6675302914adbe27d0f739bb9c86b21bc76
Hub5on <haibert.hs32@gmail.com>
2023-10-18 18:53:11 +0200

    add: add task numbering and 'Done' status editing

--- Todo.java
+++ Todo.java
@@ -6,7 +6,9 @@ import java.util.Hashtable;
 public class Todo {
     public static void main(String[] args) {
         Boolean exitApplication = false;
-        Dictionary<String, Boolean> tasks = new Hashtable<String, Boolean>();
+        Dictionary<Integer, String> tasks = new Hashtable<Integer, String>();
+        Dictionary<Integer, Boolean> taskStatus = new Hashtable<Integer, Boolean>();
+        int taskCounter = 1;
 
         try (Scanner scanner = new Scanner(System.in)) {
             do {
@@ -22,7 +24,7 @@ public class Todo {
                         case 1:
                             System.out.print("\033[H\033[2J");
                             System.out.flush();
-                            checkTodoList(tasks);
+                            checkTodoList(tasks, taskStatus, scanner);
                             System.out.println("Press Enter to return to menu");
                             scanner.nextLine();
                             break;
@@ -30,9 +32,11 @@ public class Todo {
                         case 2:
                             System.out.print("\033[H\033[2J");
                             System.out.flush();
-                            addNewTask(scanner, tasks);
+                            addNewTask(scanner, tasks, taskStatus, taskCounter);
+                            taskCounter++;
                             break;
                         case 3:
+                            // Do nothing in this version
                             break;
                         case 4:
                             exitApplication = true;
@@ -56,31 +60,51 @@ public class Todo {
         System.out.println("4. Exit");
     }
 
-    public static void addNewTask(Scanner scanner, Dictionary<String, Boolean> tasks) {
+    public static void addNewTask(Scanner scanner, Dictionary<Integer, String> tasks, Dictionary<Integer, Boolean> taskStatus, int taskCounter) {
         System.out.println("Write your task todo (or type 'exit' to finish): ");
         System.out.print("> ");
         String task = scanner.nextLine();
         if ("exit".equalsIgnoreCase(task)) {
             return;
         }
-        tasks.put(task, false);
+        tasks.put(taskCounter, task);
+        taskStatus.put(taskCounter, false);
     }
 
-    public static void checkTodoList(Dictionary<String, Boolean> tasks) {
-        System.out.println("+-------------------+---------+");
-        System.out.println("|        Task       |   Done  |");
-        System.out.println("+-------------------+---------+");
-    
-        Enumeration<String> keys = tasks.keys();
+    public static void checkTodoList(Dictionary<Integer, String> tasks, Dictionary<Integer, Boolean> taskStatus, Scanner scanner) {
+        System.out.println("+---+-------------------+---------+");
+        System.out.println("| # |        Task       |   Done  |");
+        System.out.println("+---+-------------------+---------+");
+
+        Enumeration<Integer> keys = tasks.keys();
         while (keys.hasMoreElements()) {
-            String task = keys.nextElement();
-            boolean completed = tasks.get(task);
+            int taskNumber = keys.nextElement();
+            String task = tasks.get(taskNumber);
+            boolean completed = taskStatus.get(taskNumber);
             String status = completed ? "Yes" : "No";
-            System.out.printf("| %-17s | %-7s |\n", task, status);
+            System.out.printf("| %-2d| %-17s | %-7s |\n", taskNumber, task, status);
         }
-    
-        System.out.println("+-------------------+---------+");
+
+        System.out.println("+---+-------------------+---------+");
+
+        System.out.println("Enter the task number you want to edit (or type 'exit' to finish editing): ");
+        System.out.print("> ");
+        int taskNumberToEdit = scanner.nextInt();
+        scanner.nextLine();
+        
+        if (tasks.get(taskNumberToEdit) != null) {
+            System.out.println("Enter the new status (Yes/No): ");
+            System.out.print("> ");
+            String newStatus = scanner.nextLine();
+            if ("Yes".equalsIgnoreCase(newStatus)) {
+                taskStatus.put(taskNumberToEdit, true);
+            } else if ("No".equalsIgnoreCase(newStatus)) {
+                taskStatus.put(taskNumberToEdit, false);
+            } else {
+                System.out.println("Invalid status. Task status remains unchanged.");
+            }
+        } else if (taskNumberToEdit != -1) {
+            System.out.println("Task number not found. Task status remains unchanged.");
+        }
     }
-    
-    
 }
